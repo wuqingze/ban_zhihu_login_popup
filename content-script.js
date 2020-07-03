294ec01f7fd779cf1bc1fadc56329914cbb80ee7
wuqingze <nongxiaolang@foxmail.com>
2020-07-04 07:39:42 +0800

    禁止知乎登录弹窗插件

---
+++ content-script.js
@@ -0,0 +1,8 @@
+﻿(function() {
+	setInterval(function(){
+		var t = document.getElementsByClassName("Button Modal-closeButton Button--plain");
+		if(t.length>0){
+			t[0].click();
+		}
+	},100);
+})();
